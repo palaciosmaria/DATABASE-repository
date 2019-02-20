c0b91d0c796b92283c21e637303e8f7880a1301e
amandaperez-porro <amandaperez-porro@10.60.83.84>
2019-02-20 17:45:06 +0100

    .

--- src/transplantation/pojo/Organ.java
+++ src/transplantation/pojo/Organ.java
@@ -1,5 +1,15 @@
 package transplantation.pojo;
+import java.io.Serializable;
 
-public class Organ {
+public class Organ implements Serializable {
+	//generar numero
+	
+	
+	
+	private Integer id;
+	private String typeorgan;
+	private Integer lifespan;
+	
 
+	
 }
