f98af6fbd66413e1cc3578cedb03cfb31474de1a
amandaperez-porro <amandaperez-porro@10.60.80.117>
2019-03-04 16:13:44 +0100

    .

--- src/UI.java
+++ src/UI.java
@@ -45,21 +45,7 @@ public class UI {
 	
 	switch(option) {
 	case 1:
-		
-		System.out.println("Introduce the doctor's info:");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("ID: ");
-		String idstring = reader.readLine();
-		int id= Integer.parseInt(idstring);
-		System.out.print("Name: ");
-		String name = reader.readLine();
-		System.out.print("Speciality: ");
-		String speciality = reader.readLine();
-		Doctor d= new Doctor (id,name, speciality);
-		manager.insertDoc(d);
-		
-		
-		
+			//crear otro menu para pedir que info quieres insertar
 	case 2:
 		
 		
@@ -86,4 +72,26 @@ public class UI {
 		
 		
 	}
+	
+	
+	public static void menuInsertDoctor() throws IOException {
+		
+		System.out.println("Introduce the doctor's info:");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("ID: ");
+		String idstring = reader.readLine();
+		int id= Integer.parseInt(idstring);
+		System.out.print("Name: ");
+		String name = reader.readLine();
+		System.out.print("Speciality: ");
+		String speciality = reader.readLine();
+		Doctor d= new Doctor (id,name, speciality);
+		manager.insertDoc(d);
+		
+	}
+	
+	
+	
+	
+	
 }
