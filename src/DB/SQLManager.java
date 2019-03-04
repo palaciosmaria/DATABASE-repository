6632cc74c0ac84baf41a6ccb0f9f9225403acdb9
elisabeth <elisabeth@10.60.90.149>
2019-03-04 16:17:39 +0100

    w

--- src/DB/SQLManager.java
+++ src/DB/SQLManager.java
@@ -134,7 +134,7 @@ public class SQLManager {
 		}
 		
 		public void selectDoc( Doctor d){
-			
+			try {
 			Statement stmt = c.createStatement();
 			String sql = "SELECT * FROM doctor";
 			ResultSet rs = stmt.executeQuery(sql);//we only ask question to the database
@@ -148,7 +148,9 @@ public class SQLManager {
 			rs.close();
 			stmt.close();//after using a statement we close it
 			System.out.println("Search finished.");
-		}
+			}
+			catch(Exception e)
+			{e.printStackTrace();}			}
 		
 		
 
