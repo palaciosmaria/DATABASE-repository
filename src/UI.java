192eea4e398eb05671ac28fb197b161d0a5ff642
palaciosmaria <m.palacios14@usp.ceu.es>
2019-03-06 18:11:24 +0100

    aaaahhhhh

--- src/UI.java
+++ src/UI.java
@@ -1,15 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-<<<<<<< HEAD
-import java.sql.SQLException;
-=======
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
->>>>>>> branch 'master' of https://github.com/palaciosmaria/DATABASE-repository
 import java.util.List;
 
 import DB.SQLManager;
@@ -29,26 +25,14 @@ public class UI {
 
 //Menu		
 
-<<<<<<< HEAD
-	
-
-	int option=0;		
-=======
 	int option = 0;		
->>>>>>> branch 'master' of https://github.com/palaciosmaria/DATABASE-repository
 	
-<<<<<<< HEAD
-
-
-		
-=======
->>>>>>> branch 'master' of https://github.com/palaciosmaria/DATABASE-repository
-	do {	
+	//do {	
 	System.out.println("------MENU------");
 
 	System.out.println("1. Insert the information");
 	System.out.println("2. Delete");
-	System.out.println("3. Search");
+	System.out.println("3. Update");
 	
 	try {
 	System.out.println("Insert the option: ");
@@ -59,42 +43,39 @@ public class UI {
 		e.printStackTrace();
 		
 	}
-	}while(option>1 || option <3);	
+	//}while(option<=1 || option >=3);	
 	
 	switch(option) {
 	case 1:
+		try{
 		
+		System.out.println("Introduce the doctor's info:");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Name: ");
+		String name = reader.readLine();
+		System.out.print("Speciality: ");
+		String speciality = reader.readLine();
+		Doctor d= new Doctor (,name, speciality);
+		manager.insertDoc(d);
+		System.out.println("Doctor inserted correctly");
+		break;
 		
-		
-		
-		
-		
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	case 2:
 		
-		
-		
-		//tst
-	
-	
-	
-	
-	
-	
-	
-	
-		
-<<<<<<< HEAD
 		
 		
 		
 	case 3:
 		try{
 			
-		reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Choose a doctor, type its ID: ");
 		List<Doctor> list1= manager.getAllDoctors();
 		System.out.println(list1);
-		id = Integer.parseInt(reader.readLine());
+		int id = Integer.parseInt(reader.readLine());
 		
 		//call a method in manager that returns a doctor by id
 		//print the chosen doctor
@@ -110,42 +91,5 @@ public class UI {
 		
 	
 	}
-=======
-	case 3:
->>>>>>> branch 'master' of https://github.com/palaciosmaria/DATABASE-repository
-		
-			
-		
-<<<<<<< HEAD
-		
-		
-	
-		
-		
 	}
-	
-	
-	public static void menuInsertDoctor() throws IOException {
-		
-		System.out.println("Introduce the doctor's info:");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("ID: ");
-		String idstring = reader.readLine();
-		int id= Integer.parseInt(idstring);
-		System.out.print("Name: ");
-		String name = reader.readLine();
-		System.out.print("Speciality: ");
-		String speciality = reader.readLine();
-		Doctor d= new Doctor (id,name, speciality);
-		manager.insertDoc(d);
-		
-	}
-	
-	
-	
-	
-	
 }
-=======
-}}}
->>>>>>> branch 'master' of https://github.com/palaciosmaria/DATABASE-repository
